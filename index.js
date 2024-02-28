0b247a40571a8f886fc73dc0a95b35c5f5072d39
Abhishek Sharma <avisheksharma2004@gmail.com>
2024-02-29 00:19:39 +0530

    MongoDB connected

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+
+const userRoute = require("./routes/user");
 
 const app = express();
-const PORT = 8000;
+const PORT = 3000;
+
+mongoose.connect("mongodb://127.0.0.1:27017/Blogify").then((e) => console.log("MongoDB Connected"));
+
+app.set("view engine" , "ejs");
+app.set("views" , path.resolve('./views'));
+
+app.get("/" , (req, res) => {
+    res.render("home");
+})
+
+app.use("/user" , userRoute);
+
 
 
 app.listen(PORT , () => {
---
+++ models/user.js
@@ -0,0 +1,53 @@
+const { createHmac , randomBytes } = require("crypto");
+const { Schema , model } = require("mongoose");
+
+const userSchema = new Schema({
+    fullName : {
+        type : String , 
+        required : true,
+    } , 
+    email : {
+        type : String , 
+        required : true,
+        unique : true ,
+    } ,
+    salt : {
+        type : String ,
+        required : true , 
+    } ,
+    password : {
+        type : String , 
+        required : true , 
+
+    } ,
+    profileImgageURL : {
+        type : String , 
+        default : "/images/default.png"
+    } , 
+    role : {
+        type : String , 
+        enum : ["USER" , "ADMIN"],
+        default : "USER" ,
+    } ,
+} , { timestamps : true }
+);
+
+userSchema.pre("save" , function(next) {
+    const user = this;
+
+    if(!user.isModified("password"))
+    return;
+
+    const salt = randomBytes(16).toString();
+    const hashedPassword = createHmac("sha256" , slat)
+    .update(user.password)
+    .digest("hex");
+
+    this.salt = salt;
+    this.password = hashedPassword;
+
+});
+
+const User = model("user" , userSchema);
+
+module.exports = User;
